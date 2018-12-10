1433eee2d46adbda66bc7b7aca4642705bd91a8c
Kha-Dinh Luong <kvl11@case.edu>
2018-12-09 21:15:00 -0500

    Add saver. Import data.

--- SRCNN.py
+++ SRCNN.py
@@ -1,9 +1,36 @@
 # THIS IS THE MAIN SCRIPT
 
 import tensorflow as tf
+import os
+from training import train
+from build_model import build_model
 
 def main():
-    pass
+    if len(sys.argv) != 11:
+        raise Exception("Inappropriate number of arguments. Require 9.")
+    training = int(sys.argv[1])
+    image_size = int(sys.argv[2])
+    ground_image_size = int(sys.argv[3])
+    num_channels = int(sys.argv[4])
+    f1 = int(sys.argv[5])
+    n1 = int(sys.argv[6])
+    n2 = int(sys.argv[7])
+    f3 = int(sys.argv[8])
+    num_epoch = int(sys.argv[9])
+    batch_size = int(sys.argv[10])
+
+    if training:
+
+
+        # Code block for getting data: assume 4-D tensor, tf.uint8, images of the same size, ground_images of the same size
+        images, ground_images = get_data() # Will implement after Yufei finishes data processing
+
+        
+        model = build_model(image_size, ground_image_size, num_channels, f1=f1,n1=n1,n2=n2,f3=f3)
+        with tf.Session() as sess:
+            train(images, ground_images, model, sess, num_epoch=num_epoch, batch_size=batch_size)
+    else:
+        pass
 
 if __name__ == '__main__':
     main()
--- training.py
+++ training.py
@@ -4,12 +4,9 @@ from build_model import build_model
 
 import tensorflow as tf
 
-def train(images, ground_images, model, learning_rate, num_epoch, batch_size, sess):
+def train(images, ground_images, model, sess, learning_rate=1, num_epoch=1, batch_size=1):
 
     # The "model" parameter is the output of the build_model() function in build_model.py
-
-    # Code block for getting data: assume 4-D tensor, tf.uint8, images of the same size, ground_images of the same size
-    images, ground_images = get_data() # Will implement after Yufei finishes data processing
 
     # MSE loss function
     loss = tf.reduce_mean(tf.square(model['ground_images'] - model['cnn']))
@@ -18,12 +15,17 @@ def train(images, ground_images, model, learning_rate, num_epoch, batch_size, se
 
     tf.initialize_all_variables().run()
 
+    saver = tf.train.Saver(max_to_keep=5)
+
     for epoch in range(num_epoch):
+        print("EPOCH:", epoch)
         num_batches = len(images) // batch_size
         for batch in range(num_batches):
             batch_images = images[batch*batch_size : (batch+1)*batch_size]
             batch_ground_images = ground_images[batch*batch_size : (batch+1)*batch_size]
 
             sess.run(optimizer, feed_dict={model['images']:batch_images,model['ground_images']:batch_ground_images})
-
-    return model['cnn']
+        # Save after every 5 epoches:
+        if epoch % 5 == 0:
+            saver.save(sess, 'saved_model/model.ckpt', global_step=epoch)
+    return
