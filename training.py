77278f44463ca34578a1e68bec9ec76379d39d35
Kha-Dinh Luong <kvl11@case.edu>
2018-12-14 17:11:17 -0500

    -save only one best model in saved_model.

--- training.py
+++ training.py
@@ -4,6 +4,8 @@ from build_model import build_model
 
 import tensorflow as tf
 
+import shutil, sys
+
 def train(images, ground_images, model, optimizer, loss, step, sess):
 
     # The "model" parameter is the output of the build_model() function in build_model.py
@@ -12,9 +14,17 @@ def train(images, ground_images, model, optimizer, loss, step, sess):
 
     #Save after every 100 steps:
     if step % 100 == 0:
-        saver = model['saver']
-        saver.save(sess, 'saved_model/model.ckpt', global_step=step)
-        f = open('saved_model/log.txt','a')
-        f.write('Step: %f.Loss = %f\n' %(step, loss_val))
+        f = open('best_loss.txt', 'r')
+        best_loss = float(f.read())
+        f.close()
+        if loss_val < best_loss:
+            sys.stdout.flush()
+            shutil.rmtree('saved_model')
+            f = open('best_loss.txt', 'w')
+            f.write('%f' %(loss_val))
+            f.close()
+            saver = model['saver']
+            saver.save(sess, 'saved_model/model.ckpt', global_step=step)
+
 
     return loss_val
